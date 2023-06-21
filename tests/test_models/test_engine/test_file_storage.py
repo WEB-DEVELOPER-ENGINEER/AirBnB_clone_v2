cc3842ba9e410c160aecc3a1652adcfe7d7e6e07
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-06-20 23:25:07 -0700

    done

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,13 +4,9 @@ import unittest
 from models.base_model import BaseModel
 from models import storage
 import os
-import time
-import json
-import sys
 
 
-@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
-                 "test only for FileStorage")
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "for FileStorage")
 class TestFileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
