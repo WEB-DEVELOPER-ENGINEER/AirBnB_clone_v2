ad68d2b3bdca1d5fe552235963195a16e21e6771
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-06-21 17:41:27 -0700

    done

--- models/place.py
+++ models/place.py
@@ -1,39 +1,56 @@
 #!/usr/bin/python3
-"""This is the place class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel
-from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
+""" Place Module """
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
 import models
+from models.review import Review
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, Float, String, ForeignKey, Table
 
 
-class Place(BaseModel):
-    """This is the class for Place
-    """
-    @property
-    def reviews(self):
-        """ Returns list of reviews.id """
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            review = key.replace('.', ' ')
-            review = shlex.split(review)
-            if (review[0] == 'Review'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.place_id == self.id):
-                result.append(elem)
-        return (result)
+if models.is_type == "db":
+    relationship_table = Table('place_amenity', Base.metadata,
+                               Column('place_id', String(60),
+                                      ForeignKey('places.id'),
+                                      nullable=False),
+                               Column('amenity_id', String(60),
+                                      ForeignKey('amenities.id'),
+                                      nullable=False))
 
-    @property
-    def amenities(self):
-        """ Returns list of amenity ids """
-        return self.amenity_ids
 
-    @amenities.setter
-    def amenities(self, obj=None):
-        """ Appends amenity ids to the attribute """
-        if type(obj) is Amenity and obj.id not in self.amenity_ids:
-            self.amenity_ids.append(obj.id)
+class Place(BaseModel, Base):
+    """ class place """
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    reviews = relationship('Review', backref='place', cascade='delete')
+    amenities = relationship('Amenity', secondary=relationship_table,
+                             viewonly=False)
+    amenity_ids = []
+
+    if models.is_type != 'db':
+        @property
+        def reviews(self):
+            """ Place reviews """
+            rv = models.storage.all(Review).values()
+            return {re for re in rv if re.place_id == self.id}
+
+        @property
+        def amenities(self):
+            """ Place amenities """
+            ob = models.storage.all(Amenity).values()
+            return [obj for obj in ob if obj.id in self.amenity_ids]
+
+        @amenities.setter
+        def amenities(self, value):
+            """ Amenities setter """
+            if type(value) is Amenity:
+                self.amenity_ids.append(value.id)
