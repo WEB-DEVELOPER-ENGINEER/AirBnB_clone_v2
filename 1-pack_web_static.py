c91c3ef0a2cb8be8eeab6de707d1240ef6329b1a
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-15 14:53:50 -0700

    done

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Fabfile to generates a .tgz archive from the contents of web_static.
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    '''
+        generates a .tgz archive from the contents of the web_static folder
+    '''
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("tar -czf {} web_static".format(filename))
+    if result.succeeded:
+        return (filename)
+    else:
+        return None
