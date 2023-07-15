efbcfc0caa6e290ff08502ecb67f741851e425d1
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-15 08:38:59 -0700

    done

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+if ! which nginx > /dev/null 2>&1; then
+    sudo apt update
+    sudo apt -y install nginx
+fi
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "Hello holberton" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sed -i '/default_server;/a location \/hbnb_static {\\n\\t\\talias \/data\/web_static\/current\/;\\n}' /etc/nginx/sites-available/default
+sudo service nginx restart
