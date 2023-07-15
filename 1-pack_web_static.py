1ba7ede99256200f61a76874db59c0e1e0ccbc75
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-15 15:18:18 -0700

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,8 +7,13 @@ from fabric.api import local
 def do_pack():
     """create .tgz archive from the contents of the web_static folder"""
     local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = "versions/web_static_{}.tgz".format(date)
+    now = datetime.now()
+    filename = "versions/web_static_{}{}{}{}{}{}.tgz".format(now.year,
+                                                             now.month,
+                                                             now.day,
+                                                             now.hour,
+                                                             now.minute,
+                                                             now.second)
     result = local("tar -czf {} web_static".format(filename))
     if result.succeeded:
         return (filename)
