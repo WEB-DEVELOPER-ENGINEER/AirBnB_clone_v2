6275506c5482e83aaf540f34076bcc192c0faff1
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-15 17:39:42 -0700

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""
-Distributes an archive to web servers
-"""
-
-from fabric.api import put, run, env
-from os.path import exists
+"""Distributes an archive to web servers"""
+from fabric.api import env, run, put, cd, sudo
+from os.path import basename, join, splitext, exists
 env.hosts = ['34.207.253.78', '3.84.239.44']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to web servers"""
+    if not exists(archive_path):
         return False
-    try:
-        file_name = archive_path.split("/")[-1]
-        no_ext = file_name.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except Exception:
-        return False
+    file_name = basename(archive_path)
+    no_ext = splitext(file_name)[0]
+    path = '/data/web_static/releases/'
+    with cd('/tmp'):
+        put(archive_path, './')
+    with cd(path):
+        sudo('mkdir -p {}/{}'.format(path, no_ext))
+        sudo('tar -xzf /tmp/{} -C {}/{}'.format(file_name, path, no_ext))
+        sudo('rm -f /tmp/{}'.format(file_name))
+        with cd('{}/{}'.format(path, no_ext)):
+            sudo('mv web_static/* .')
+            sudo('rm -rf web_static')
+    with cd('/data/web_static'):
+        sudo('rm -f current')
+        sudo('ln -sf {}/{} current'.format(path, no_ext))
+    return True
