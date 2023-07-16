7ac4055798fb6887e5e8c07cd424e7b3033c0d0b
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-15 17:26:22 -0700

    done

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,7 +11,7 @@ def do_pack():
     local("mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "versions/web_static_{}.tgz".format(date)
-    result = local("tar -vzfc {} web_static/".format(filename))
+    result = local("tar -cvzf {} web_static/".format(filename))
     if result.succeeded:
         return (filename)
     else:
