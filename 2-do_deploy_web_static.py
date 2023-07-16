3cf0aa6d26546baef5caf5c6dc8b8d9cd49db71e
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-07-15 17:48:55 -0700

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,99 @@
-#!/usr/bin/python3
-"""Distributes an archive to web servers"""
-from fabric.api import env, run, put, cd, sudo
-from os.path import basename, join, splitext, exists
-env.hosts = ['34.207.253.78', '3.84.239.44']
+#!/usr/bin/env python3
 
+"""
+Fabric script to deploy an archive to web servers.
+"""
 
-def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
-    if not exists(archive_path):
+import os
+import datetime
+from fabric.api import env, put, run, cd
+from fabric.contrib.files import exists
+
+# Set the web server IP addresses
+env.hosts = ["35.243.225.26", "34.75.87.53"]
+
+# Set the remote directory for storing the archive
+env.remote_dir = "/data/web_static/releases/"
+
+# Set the name of the archive file
+env.archive_name = "web_static"
+
+# Set the directory for storing error logs
+env.error_log_dir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "logs"
+)
+
+def _get_timestamp():
+    """Get the current timestamp in the format YYYYMMDDHHMMSS"""
+
+    now = datetime.datetime.now()
+    return now.strftime("%Y%m%d%H%M%S")
+
+def _get_remote_archive_path():
+    """Get the path to the remote archive file"""
+
+    timestamp = _get_timestamp()
+    return os.path.join(env.remote_dir, env.archive_name + "_" + timestamp + ".tgz")
+
+def _create_remote_dir():
+    """Create the remote directory for storing the archive"""
+
+    run("sudo mkdir -p {}".format(env.remote_dir))
+
+def _upload_archive(archive_path):
+    """Upload the archive to the remote server"""
+
+    remote_archive_path = _get_remote_archive_path()
+    put(archive_path, remote_archive_path)
+    return remote_archive_path
+
+def _extract_archive(remote_archive_path):
+    """Extract the archive on the remote server"""
+
+    remote_dir = os.path.join(
+        env.remote_dir, os.path.splitext(os.path.basename(remote_archive_path))[0]
+    )
+
+    run("sudo mkdir -p {}".format(remote_dir))
+    with cd(remote_dir):
+        run("sudo tar -xzf {}".format(remote_archive_path))
+
+    return remote_dir
+
+def _create_symlink(remote_dir):
+    """Create a symbolic link to the new release"""
+
+    run("sudo rm -f /data/web_static/current")
+    run("sudo ln -s {} /data/web_static/current".format(remote_dir))
+
+def deploy(archive_path):
+    """Deploy the archive to the web servers"""
+
+    if not os.path.exists(archive_path):
+        print("ERROR: Archive file not found: {}".format(archive_path))
         return False
-    file_name = basename(archive_path)
-    no_ext = splitext(file_name)[0]
-    path = '/data/web_static/releases/'
-    with cd('/tmp'):
-        put(archive_path, './')
-    with cd(path):
-        sudo('mkdir -p {}/{}'.format(path, no_ext))
-        sudo('tar -xzf /tmp/{} -C {}/{}'.format(file_name, path, no_ext))
-        sudo('rm -f /tmp/{}'.format(file_name))
-        with cd('{}/{}'.format(path, no_ext)):
-            sudo('mv web_static/* .')
-            sudo('rm -rf web_static')
-    with cd('/data/web_static'):
-        sudo('rm -f current')
-        sudo('ln -sf {}/{} current'.format(path, no_ext))
-    return True
+
+    try:
+        # Create the remote directory for storing the archive
+        _create_remote_dir()
+
+        # Upload the archive to the remote server
+        remote_archive_path = _upload_archive(archive_path)
+
+        # Extract the archive on the remote server
+        remote_dir = _extract_archive(remote_archive_path)
+
+        # Create a symbolic link to the new release
+        _create_symlink(remote_dir)
+
+        print("Deployment successful!")
+        return True
+
+    except Exception as e:
+        error_log_path = os.path.join(
+            env.error_log_dir, "deploy_error_{}.log".format(_get_timestamp())
+        )
+        with open(error_log_path, "w") as f:
+            f.write(str(e))
+        print("ERROR: Deployment failed. See {} for details.".format(error_log_path))
+        return False
