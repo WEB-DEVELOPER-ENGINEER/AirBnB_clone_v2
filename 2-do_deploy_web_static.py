8eeba2cdf2705c88ee30ad2e3b8ba828cf6dab88
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-08-06 09:04:34 -0700

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ import os.path
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
-
+from fabric.api import sudo
 env.hosts = ["34.207.253.78", "3.84.239.44"]
 
 
@@ -23,25 +23,25 @@ def do_deploy(archive_path):
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
     if sudo("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+            format(name)).failed is True:
         return False
     if sudo("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
+            format(name)).failed is True:
         return False
     if sudo("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
+            format(file, name)).failed is True:
         return False
     if sudo("rm /tmp/{}".format(file)).failed is True:
         return False
     if sudo("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+            "/data/web_static/releases/{}/".format(name, name)).failed is True:
         return False
     if sudo("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
+            format(name)).failed is True:
         return False
     if sudo("rm -rf /data/web_static/current").failed is True:
         return False
     if sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+            format(name)).failed is True:
         return False
     return True
