020f86416bce3e58bf2636bc75294c7bbad6c781
WEB-DEVELOPER-ENGINEER <mohamedaly6600@yahoo.com>
2023-08-09 19:10:27 -0700

    done

---
+++ web_flask/9-states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Starts a Flask web app listening on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """/states_list route"""
+    states_list = list(storage.all(State).values())
+    return render_template('9-states.html', states=states_list)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_cities_list(id=None):
+    """/cities_by_states route"""
+    e = None
+    states = storage.all(State)
+    c = list(storage.all(City).values())
+    if id:
+        key = "State." + id
+        if key in states.keys():
+            e = states[key]
+    return render_template('9-states.html', cities=c, state=e, id=id)
+
+
+@app.teardown_appcontext
+def close(self):
+    """Closes session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
